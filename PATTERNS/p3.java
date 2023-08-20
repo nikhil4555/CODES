06243d0f622927e86dee2bc5312994741dd461fc
nikhil4555 <nikhil.20bce7200@vitap.ac.in>
2023-08-20 22:32:25 +0530

    equilateraltriangle

--- PATTERNS/p3.java
+++ PATTERNS/p3.java
@@ -4,15 +4,16 @@ public class p3 {
     public static void main(String args[])
     {
        // int arr[][]=new int[10][10];
-        for(int i=0;i<5;i++)
+        for(int i=0;i<5;i++) //passes
         {
-            for(int j=0;j<=5-i;j++)
+            for(int j=0;j<=5-i;j++) // spaces
             {
                 System.out.print(" ");
             }
-            for(int k=0;k<=i;k++)
+            for(int k=0;k<=i;k++) // printing the symbol
             System.out.print("*");
-            System.out.println(" ");
+
+            System.out.println(" ");// for the next line
         }
     }
 }
